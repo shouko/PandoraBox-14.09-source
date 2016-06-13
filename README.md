fc63a48626f6ad2dbc59a22670325951504d5c10
sysorem <onlyweloving@gmail.com>
2016-06-13 23:42:06 +0800

    ipk

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # PandoraBox-14.09-source
-小米路由器青春版PandoraBox 14.09源
+小米路由器青春版(MT7620)PandoraBox 14.09源(ipk)
+
+我的配置信息
+小米路由器 青春版 
+PandoraBox软件源 
+主机名 PandoraBox 
+主机型号 Xiaomi R1CL Board
+固件版本 PandoraBox 14.09 r1468 / LuCI 0.12 (0.12+git-4d0a20b) 
+内核版本 3.14.52 
+
+也可以直接添加源
+源设置：src/gz packages http://pandorabox.sysorem.xyz/ralink/mt7620/packages/
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+    echo "小米路由器 青春版 PandoraBox软件源<br />";
+    echo "主机名 PandoraBox<br />";
+    echo "主机型号    Xiaomi R1CL Board";
+    echo "固件版本    PandoraBox 14.09 r1468 / LuCI 0.12 (0.12+git-4d0a20b)<br />";
+    echo "内核版本    3.14.52<br />";
+    echo "源设置：";
+    echo "src/gz packages http://pandorabox.sysorem.xyz/ralink/mt7620/packages/<br />";
